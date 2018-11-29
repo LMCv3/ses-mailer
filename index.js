a163a179f6c833ecd30f77593cadedeb6172f3c5
Louis McCoy <louie@wellandlighthouse.com>
2018-11-29 15:21:35 -0600

    Tabs > Spaces

--- index.js
+++ index.js
@@ -2,49 +2,48 @@ const AWS = require("aws-sdk");
 const program = require('commander');
 const inquirer = require('inquirer');
 if (typeof Promise === 'undefined') {
-  AWS.config.setPromisesDependency(require('bluebird'));
+	AWS.config.setPromisesDependency(require('bluebird'));
 }
 
-
 AWS.config.update({
-    accessKeyId: <<YOUR_ACCESS_KEY>>,
-    secretAccessKey: <<YOUR_ACCESS_KEY>>,
-    region: <<YOUR_ACCESS_KEY>>
-  });
+	accessKeyId: <<YOUR_ACCESS_KEY>>,
+	secretAccessKey: <<YOUR_ACCESS_KEY>>,
+	region: <<YOUR_ACCESS_KEY>>
+});
 
 const ses = new AWS.SES({ apiVersion: "2010-12-01" });
 const params = {
-  Destination: {
-    ToAddresses: [""XYZ@XYZ.com""] // Email address/addresses that you want to send your email
-  },
-  ConfigurationSetName: <<ConfigurationSetName>>,
-  Message: {
-    Body: {
-      Html: {
-        // HTML Format of the email
-        Charset: "UTF-8",
-        Data:
-          "<html><body><h1>Hello  Charith</h1><p style='color:red'>Sample description</p> <p>Time 1517831318946</p></body></html>"
-      },
-      Text: {
-        Charset: "UTF-8",
-        Data: "Hello Charith Sample description time 1517831318946"
-      }
-    },
-    Subject: {
-      Charset: "UTF-8",
-      Data: "Test email"
-    }
-  },
-  Source: "ABC@ABC.com"
+	Destination: {
+		ToAddresses: [""XYZ@XYZ.com""] // Email address/addresses that you want to send your email
+	},
+	ConfigurationSetName: <<ConfigurationSetName>>,
+	Message: {
+		Body: {
+			Html: {
+				// HTML Format of the email
+				Charset: "UTF-8",
+				Data:
+					"<html><body><h1>Hello  Charith</h1><p style='color:red'>Sample description</p> <p>Time 1517831318946</p></body></html>"
+			},
+			Text: {
+				Charset: "UTF-8",
+				Data: "Hello Charith Sample description time 1517831318946"
+			}
+		},
+		Subject: {
+			Charset: "UTF-8",
+			Data: "Test email"
+		}
+	},
+	Source: "ABC@ABC.com"
 };
 
 const sendEmail = ses.sendEmail(params).promise();
 
 sendEmail
-  .then(data => {
-    console.log("email submitted to SES", data);
-  })
-  .catch(error => {
-    console.log(error);
+	.then(data => {
+		console.log("email submitted to SES", data);
+	})
+	.catch(error => {
+		console.log(error);
 });
